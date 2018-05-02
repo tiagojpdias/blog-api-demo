e436ed166873988aeb3154e2b76b9a28497a8e79
Quetzy Garcia <quetzyg@altek.org>
2018-05-02 18:31:40 +0100

    feat(Filters): add service provider

---
+++ app/Providers/FilterServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use App\Filters\PostEloquentFilter;
+use App\Filters\PostFilter;
+use Illuminate\Support\ServiceProvider;
+
+class FilterServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $filters = [
+            PostFilter::class => PostEloquentFilter::class,
+        ];
+
+        foreach ($filters as $interface => $concrete) {
+            $this->app->singleton($interface, function () use ($concrete) {
+                return new $concrete;
+            });
+        }
+    }
+}
