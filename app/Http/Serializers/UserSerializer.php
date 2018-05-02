183c2c57b76794c6b4852daa34871134aa49607f
Quetzy Garcia <quetzyg@altek.org>
2018-05-02 12:34:29 +0100

    feat(Users): initial serializer implementation

---
+++ app/Http/Serializers/UserSerializer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Serializers;
+
+use App\Models\User;
+use Tobscure\JsonApi\AbstractSerializer;
+use Tobscure\JsonApi\Collection;
+use Tobscure\JsonApi\Relationship;
+
+class UserSerializer extends AbstractSerializer
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $type = 'users';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAttributes($model, array $fields = null): array
+    {
+        return [
+            'name'       => $model->name,
+            'email'      => $model->email,
+            'created_at' => $model->created_at->toDateTimeString(),
+            'updated_at' => $model->updated_at->toDateTimeString(),
+        ];
+    }
+
+    /**
+     * Blog Posts.
+     *
+     * @param User $user
+     *
+     * @return \Tobscure\JsonApi\Relationship
+     */
+    public function questions(User $user): Relationship
+    {
+        return new Relationship(new Collection($user->posts, new PostSerializer()));
+    }
+}
