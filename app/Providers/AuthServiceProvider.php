f86c3a95d502edd1f85d1fa98eee877943044005
Quetzy Garcia <quetzyg@altek.org>
2018-05-03 14:02:24 +0100

    fix(Auth): remove unused import

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -22,7 +21,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
     }
 }
