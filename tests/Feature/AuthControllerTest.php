bc214aff67f4e0b6e6b738dc18282416425ec1a8
Quetzy Garcia <quetzyg@altek.org>
2018-05-05 14:24:31 +0100

    feat(Auth): add register endpoint tests

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -8,7 +8,69 @@ use Tests\TestCase;
 
 class AuthControllerTest extends TestCase
 {
+    use ApiUser;
     use RefreshDatabase;
+
+    /**
+     * @group auth::register
+     * @test
+     */
+    public function itWillNotRegisterDueToValidationErrors(): void
+    {
+        $response = $this->json('POST', route('auth.register'), [
+            'name'     => str_repeat('foo', 100),
+            'email'    => 'something else entirely',
+            'password' => 123,
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJson([
+            'errors' => [
+                [
+                    'id'     => 'name',
+                    'detail' => 'The name may not be greater than 255 characters.',
+                ],
+                [
+                    'id'     => 'email',
+                    'detail' => 'The email must be a valid email address.',
+                ],
+                [
+                    'id'     => 'password',
+                    'detail' => 'The password confirmation does not match.',
+                ],
+            ],
+        ]);
+    }
+
+    /**
+     * @group auth::register
+     * @test
+     */
+    public function itWillSuccessfullyRegister(): void
+    {
+        $response = $this->json('POST', route('auth.register'), [
+            'name'                  => 'John Doe',
+            'email'                 => 'john.doe@email.com',
+            'password'              => 's3cr3t',
+            'password_confirmation' => 's3cr3t',
+        ], [
+            'Authorization' => sprintf('Bearer %s', $this->generateApiUserToken()),
+        ]);
+
+        $response->assertStatus(201);
+        $response->assertJsonStructure([
+            'data' => [
+                'type',
+                'id',
+                'attributes' => [
+                    'name',
+                    'email',
+                    'created_at',
+                    'updated_at',
+                ],
+            ],
+        ]);
+    }
 
     /**
      * @group auth::authenticate
